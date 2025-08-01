2e7a4f84976680eebac9d1d8a6a53f77babb3072
Akshay Rathore <akshayrathore9010@gmail.com>
2025-08-01 12:55:37 +0530

    add update github url

--- 08-React-Router/src/components/footer/Footer.jsx
+++ 08-React-Router/src/components/footer/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/akshayrathore1197"
+                    href="https://github.com/akshayrathore11"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
